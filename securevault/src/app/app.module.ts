03f7e843b296397488bf290805a933a00c36f2d0
JornevanHelvert <jorne.van.helvert@gmail.com>
2018-03-22 09:51:40 +0100

    updated first Login page

--- securevault/src/app/app.module.ts
+++ securevault/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import { LogoComponent } from './logo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    LogoComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LogoComponent]
 })
 export class AppModule { }
