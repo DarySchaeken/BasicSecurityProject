4b8c0ba00a629c7709e3bdd078494982ddec7312
JornevanHelvert <jorne.van.helvert@gmail.com>
2018-04-19 10:06:40 +0200

    added firebase

--- securevault/src/app/app.module.ts
+++ securevault/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import {BrowserModule} from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { LoginComponent } from './Login/login.component';
 import { AppComponent } from './app.component';
 import { LeftMessagingPaneComponent } from './left-messaging-pane/left-messaging-pane.component';
 import {RouterModule} from '@angular/router';
 import { MessageContainerComponent } from './message-container/message-container.component';
 import { RightMessagingPaneComponent } from './right-messaging-pane/right-messaging-pane.component';
+import {environment} from '../environments/environment';
 
 
 @NgModule({
@@ -23,7 +26,10 @@ import { RightMessagingPaneComponent } from './right-messaging-pane/right-messag
         RouterModule.forRoot([
                 { path: '', component: LoginComponent, pathMatch: 'full', data: { title: 'Securevault | Login' } },
                 { path: 'message', component: MessageContainerComponent , data: { title: 'Securevault | Message'} }
-                ])
+                ]),
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule
     ],
   providers: [],
   bootstrap: [AppComponent]
