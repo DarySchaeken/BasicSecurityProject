94798032eb81dcb182851f76ac8c6f4e47c2d137
DarySchaeken <dary.schaeken@student.pxl.be>
2018-05-20 13:05:57 +0200

    Completed message encryption and decryption
    This includes checking the hash.
    Test code in Main() has to be removed before production.

--- demo/src/main/java/be/basicsecurity/securevault/controllers/AccountsController.java
+++ demo/src/main/java/be/basicsecurity/securevault/controllers/AccountsController.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- demo/src/main/java/be/basicsecurity/securevault/models/Message.java
+++ demo/src/main/java/be/basicsecurity/securevault/models/Message.java
@@ -19,7 +19,7 @@ import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
+import java.util.Arrays;
 import java.util.Random;
 
 import javax.crypto.BadPaddingException;
@@ -29,12 +29,14 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
 
 // SOURCES: http://www.novixys.com/blog/rsa-file-encryption-decryption-java/
 // SOURCES: http://www.novixys.com/blog/using-aes-rsa-file-encryption-decryption-java/
@@ -52,12 +54,86 @@ public class Message {
 	private File encryptedMessage;
 	@Lob
 	private byte[] encryptedHash;
+	@Transient
+	private byte[] signature;
 
-	static private Base64.Encoder encoder = Base64.getEncoder();
 	static SecureRandom srandom = new SecureRandom();
+
+	public static File decryptMessage(Message message) {
+		try {
+			byte[] bytesPublicKey = Files.readAllBytes(message.getSender().getPublicKey().toPath());
+			X509EncodedKeySpec ks = new X509EncodedKeySpec(bytesPublicKey);
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+			PublicKey pub = kf.generatePublic(ks);
+
+			byte[] bytesPrivateKey = Files.readAllBytes(message.getReceiver().getPrivateKey().toPath());
+			PKCS8EncodedKeySpec privks = new PKCS8EncodedKeySpec(bytesPrivateKey);
+			KeyFactory privkf = KeyFactory.getInstance("RSA");
+			PrivateKey pvt = privkf.generatePrivate(privks);
+
+			FileInputStream in = new FileInputStream(message.getEncryptedMessage());
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.DECRYPT_MODE, pvt);
+			byte[] b = new byte[256];
+			in.read(b);
+			byte[] keyb = cipher.doFinal(b);
+			SecretKeySpec skey = new SecretKeySpec(keyb, "AES");
+
+			byte[] iv = new byte[128 / 8];
+			in.read(iv);
+			IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+			String fileBase = Paths.get(System.getProperty("user.home"), "/decryptedIsBetter").toString();
+			File decryptedMessage = new File(fileBase + "/" + message.getEncryptedMessage().getName() + ".txt");
+			if (!decryptedMessage.getParentFile().exists()) {
+				decryptedMessage.getParentFile().mkdirs();
+			}
+
+			Cipher ci = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			ci.init(Cipher.DECRYPT_MODE, skey, ivspec);
+			try (FileOutputStream out = new FileOutputStream(decryptedMessage)) {
+				processFile(ci, in, out);
+			}
+
+			MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+			messageDigest.update(Files.readAllBytes(decryptedMessage.toPath()));
+			byte[] testSignature = messageDigest.digest();
+			Cipher signCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			signCipher.init(Cipher.DECRYPT_MODE, pub);
+			byte[] decryptedSignature = signCipher.doFinal(message.encryptedHash);
+			
+
+			if (Arrays.equals(decryptedSignature, testSignature)) {
+				System.out.println("ok!");
+				return decryptedMessage;
+			} else {
+				System.out.println("Someone tried to fool you!");
+			}
+			return null;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 	public Message(Account sender, Account receiver, File message) {
 		try {
+			this.receiver = receiver;
+			this.sender = sender;
 			KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
 			keyGenerator.init(128);
 			SecretKey secretKey = keyGenerator.generateKey();
@@ -84,9 +160,9 @@ public class Message {
 
 			MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
 			messageDigest.update(Files.readAllBytes(message.toPath()));
-			byte[] signature = messageDigest.digest();
+			signature = messageDigest.digest();
 
-			Cipher signCipher = Cipher.getInstance("RSA");
+			Cipher signCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 			signCipher.init(Cipher.ENCRYPT_MODE, pvt);
 			encryptedHash = signCipher.doFinal(signature);
 
