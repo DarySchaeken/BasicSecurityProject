e6551e1215a57d3517ab923dd8d3130d6247b49c
LotteStockmans <lotte.stockmans@gmail.com>
2018-05-20 15:38:58 +0200

    Implementing the MessagesController

--- demo/src/main/java/be/basicsecurity/securevault/controllers/MessagesController.java
+++ demo/src/main/java/be/basicsecurity/securevault/controllers/MessagesController.java
@@ -8,6 +8,38 @@ public class MessagesController {
     @Autowired
     private MessageRepository messageRepository;
 
+    @Autowired
+    private ObjectMapper objectMapper;
 
+    @PostMapping("/sentmessage")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String create(@RequestBody Map<String, Object> json) {
+        File encyptedMessage = json.get("encryptedMessage").toString();
+        Account sender = json.get("username");
+        Accound receiver = json.get("receiver");
+        Message message = new Message(sender, receiver, message);
+        messageRepository.save(message);
+        return "Message sent!";
+    }
+
+    @GetMapping("/{getmessage}")
+    public String get(@PathVariable("id") String id) {
+        try {
+            return messageRepository.findById(id).get().get().getEncryptedMessage();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Message not found!";
+        }
+    }
+
+    @GetMapping
+    public String list() {
+        try {
+            return objectMapper.writeValueAsString(messageRepository.findAll());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "No Messages!";
+        }
+    }
 
 }
