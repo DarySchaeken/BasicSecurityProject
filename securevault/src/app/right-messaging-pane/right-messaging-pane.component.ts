b0c0b6ca10c9c07aa51a2981eb7aae43e908e574
JornevanHelvert <jorne.van.helvert@gmail.com>
2018-05-22 17:18:07 +0200

    File upload should work

--- securevault/src/app/right-messaging-pane/right-messaging-pane.component.ts
+++ securevault/src/app/right-messaging-pane/right-messaging-pane.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-right-messaging-pane',
   templateUrl: './right-messaging-pane.component.html',
   styleUrls: ['./right-messaging-pane.component.css']
 })
-export class RightMessagingPaneComponent implements OnInit {
+export class RightMessagingPaneComponent {
+    selectedFile: File = null;
+    fileUploadUrl = environment.apiUrl;
 
-  constructor() { }
+    constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-  }
+    selectedFileChanged(event) {
+        this.selectedFile = <File>event.target.files[0];
+    }
 
+    sendFile() {
+      // Voor Formdata
+      const formData = new FormData();
+      formData.append('upload', this.selectedFile, this.selectedFile.name);
+      this.http.post(this.fileUploadUrl, formData).subscribe(res => {
+        console.log(res);
+      });
+      /* Voor binary input als back-end dat toelaat
+        this.http.post(this.fileUploadUrl, this.selectedFile).subscribe(res => {
+            console.log(res);
+        });
+      */
+    }
 }
