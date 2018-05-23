b85cfd9914047a176bc3519db5b854ad4721cc27
JornevanHelvert <jorne.van.helvert@gmail.com>
2018-05-23 16:30:33 +0200

    .

--- securevault/src/app/Login/login.component.ts
+++ securevault/src/app/Login/login.component.ts
@@ -42,6 +42,7 @@ export class LoginComponent implements OnInit {
 
 
     onClick(): void {
+        this.userName.nativeElement.focus();
         if (this.login.nativeElement.style.visibility !== 'visible') {
             this.makeLoginVisible();
         } else if (this.userNameCheck && this.passwordCheck) {
