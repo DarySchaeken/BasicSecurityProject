f4ce887969f933cb9ff12ee46d9c5d3a907d876f
JornevanHelvert <jorne.van.helvert@gmail.com>
2018-05-23 16:46:25 +0200

    .

--- securevault/src/app/message-container/message-container.component.ts
+++ securevault/src/app/message-container/message-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-message-container',
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageContainerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {
+      this.renderer.setStyle(document.body, 'background-color', '#FFFFFF');
+  }
 
   ngOnInit() {
   }
